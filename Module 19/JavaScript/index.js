cb88b42049858a54d3af62b788ff53801cbb5904
ecnalezny <161167580+ecnalezny@users.noreply.github.com>
2024-08-19 19:45:49 -0500

    alert added

--- Module 19/JavaScript/index.js
+++ Module 19/JavaScript/index.js
@@ -1,7 +1,14 @@
+//Alert-- going to new page
+$(document).ready(function(){
+    $("#b1").click(function(){
+        alert("You are leaving this page!");
+    });
+});
+
 //PRE LOADER
 function fade(){
     $(".preloader").fadeOut("slow");
 }
 setTimeout(fade, 2000);
 
-//STYLE CHANGER
+
